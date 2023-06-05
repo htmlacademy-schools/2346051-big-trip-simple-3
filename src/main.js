f694587b27836ccc54f712aa83cb5432b46f3143
Vladislav Mayantsev <Vladislav678300@mail.ru>
2023-06-05 21:46:20 +0300

    module4-task2

--- src/main.js
+++ src/main.js
@@ -1,17 +1,19 @@
 import FilterView from './view/filter-view.js';
-import {render} from './framework/render.js';
+import { render } from './framework/render.js';
 import BoardPresenter from './presenter/board-presenter.js';
 import ModelPoint from './model/model-point.js';
 import { mockInit, tripPoints } from './mock/point.js';
+import { generateFilter } from './mock/filter.js';
+import { generateSorter } from './mock/sort.js';
 
 const pageContainer = document.querySelector('.trip-events');
 const siteFilterElement = document.querySelector('.trip-controls__filters');
 mockInit(5, 10);
-const tripPointsModel = new ModelPoint(tripPoints);
-const boardPresenter = new BoardPresenter({boardContainer: pageContainer, tripPointsModel});
+const filters = generateFilter();
+const sorters = generateSorter();
 
-render(new FilterView(), siteFilterElement);
+const tripPointsModel = new ModelPoint(tripPoints);
+const boardPresenter = new BoardPresenter({boardContainer: pageContainer, tripPointsModel, sorters});
+render(new FilterView(filters), siteFilterElement);
 
 boardPresenter.init();
-
-// complete
